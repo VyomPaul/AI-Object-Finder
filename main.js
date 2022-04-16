777b82d85df593fb4db856dad079f19fa74a8ec1
VyomPaul <86235294+VyomPaul@users.noreply.github.com>
2022-04-16 14:28:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 object = "";
 status = "";
+results = [];
 
 function setup() {
     canvas.creaateCanvas(480, 380);
@@ -23,4 +24,24 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 480, 380);
+    if(status != "") {
+        for(i = 0; i<objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function gotResult() {
+    if(error) {
+        console.log(error);
+    }
+    console.log(results)
 }
